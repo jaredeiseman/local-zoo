7b3d03977489ad9c6d073b68f04b8d2e59c399b3
Jared Eiseman <jathei@gmail.com>
2017-07-07 08:36:03 -0700

    write Animal model, add animals property to AppComponent to house animals added to animals

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-
+  animals: Animal[];
 }
